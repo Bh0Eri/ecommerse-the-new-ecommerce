59f1c6d1763f3ba4d5e89195a8172127d3fcd7a5
Bh0Eri <ericmoreiradosreis@gmail.com>
2026-03-18 11:11:49 -0300

    feat: User controller

---
+++ src/main/java/com/projeto/ecommerce/controller/UserController.java
@@ -0,0 +1,46 @@
+package com.projeto.ecommerce.controller;
+
+import com.projeto.ecommerce.dto.UserEntityDto;
+import com.projeto.ecommerce.entities.UserEntity;
+import com.projeto.ecommerce.services.user.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/users")
+@RequiredArgsConstructor
+public class UserController {
+
+    private final UserService userService;
+
+    @PostMapping
+    public ResponseEntity<UserEntityDto> create(@RequestBody UserEntityDto dto){
+        return ResponseEntity.ok(userService.create(dto));
+    }
+
+    @GetMapping
+    public ResponseEntity<List<UserEntity>> showAll(){
+        return ResponseEntity.ok(userService.showall());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Optional<UserEntityDto>> showOne(@PathVariable UUID id){
+        return ResponseEntity.ok(userService.showone(id));
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<UserEntityDto> update(@PathVariable UUID id, @RequestBody UserEntityDto dto){
+        return ResponseEntity.ok(userService.update(id, dto));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> delete(@PathVariable UUID id){
+        userService.delete(id);
+        return ResponseEntity.noContent().build();
+    }
+}
