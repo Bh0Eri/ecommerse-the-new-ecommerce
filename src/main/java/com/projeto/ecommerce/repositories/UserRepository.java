81e6e88572d690f690cbbb99d2412d081d1fcaed
Bh0Eri <ericmoreiradosreis@gmail.com>
2026-05-20 09:58:07 -0300

    feat: fixed UserRepository

--- src/main/java/com/projeto/ecommerce/repositories/UserRepository.java
+++ src/main/java/com/projeto/ecommerce/repositories/UserRepository.java
@@ -4,12 +4,12 @@ import com.projeto.ecommerce.entities.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface UserRepository extends JpaRepository<UserEntity, UUID> {
 
-    List<UserEntity> findAll();
+    Optional<UserEntity> findByEmail(String email);
 
 }
